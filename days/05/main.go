fb3b2d0d2cb1dd75a70004de31f6a50e31288fad
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-12-14 09:26:07 +0100

    docs(Day 05): Add perf tracking comment

--- days/05/main.go
+++ days/05/main.go
@@ -17,13 +17,13 @@ func main() {
     polymers := string(dat)
 
     start := time.Now()
-    resultPartOne := solver.SolvePartOne(polymers)
+    resultPartOne := solver.SolvePartOne(polymers) // 11264 (467.728697ms)
     t := time.Now()
     elapsed := t.Sub(start)
     fmt.Printf("Answer part one: %d (%+v)\n", len(resultPartOne), elapsed)
 
     start = time.Now()
-    resultPartTwo := solver.SolvePartTwo(polymers)
+    resultPartTwo := solver.SolvePartTwo(polymers) // 4552 (13.427258909s)
     t = time.Now()
     elapsed = t.Sub(start)
     fmt.Printf("Answer part two: %d (%+v)\n", resultPartTwo, elapsed)
