b9ab2b4d7253c762daf85aca7f483d9381c24529
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2019-01-21 13:37:14 +0100

    feat(Day 10): Add parser to build each Point

---
+++ days/10/solver/solver.go
@@ -0,0 +1,30 @@
+package solver
+
+import (
+    "strings"
+    "strconv"
+)
+
+type Point struct {
+    px int
+    py int
+    vx int
+    vy int
+}
+
+func SolvePartOne(inputs []string) string {
+    return ""
+}
+
+func parse(lign string) Point {
+    slice := strings.Split(lign, "<")
+    pos := strings.Split(strings.Split(slice[1], ">")[0], ",")
+    vel := strings.Split(strings.Split(slice[2], ">")[0], ",")
+
+    px, _ := strconv.Atoi(strings.Trim(pos[0], " "))
+    py, _ := strconv.Atoi(strings.Trim(pos[1], " "))
+    vx, _ := strconv.Atoi(strings.Trim(vel[0], " "))
+    vy, _ := strconv.Atoi(strings.Trim(vel[1], " "))
+
+    return Point{px, py, vx, vy}
+}
